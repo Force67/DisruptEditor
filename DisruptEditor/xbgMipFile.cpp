dda2ac91b83fbe8f6e390c35be77a47153b8e327
Jonathan Scott <j301scott@gmail.com>
2019-04-01 10:39:36 -0700

    Add xbgMip parsing

--- DisruptEditor/xbgMipFile.cpp
+++ DisruptEditor/xbgMipFile.cpp
@@ -1,5 +1,26 @@
 #include "xbgMipFile.h"
 
-void xbgMipFile::open(IBinaryArchive & fp) {
+#include "IBinaryArchive.h"
+#include "Serialization.h"
+#include <SDL.h>
 
+void xbgMipFile::open(IBinaryArchive & fp) {
+	uint32_t magic = 1196247376;
+	fp.serialize(magic);
+	SDL_assert_release(magic == 1196247376);
+
+	uint32_t one = 1;
+	fp.serialize(unk1);
+	SDL_assert_release(one == 1);
+
+	fp.serialize(unk1);
+
+	fp.serializeNdVectorExternal(buffers);
+
+	SDL_assert_release(fp.tell() == fp.size());
+}
+
+void xbgMipFile::registerMembers(MemberStructure & ms) {
+	REGISTER_MEMBER(unk1);
+	REGISTER_MEMBER(buffers);
 }
--- DisruptEditor/xbgMipFile.h
+++ DisruptEditor/xbgMipFile.h
@@ -8,5 +8,9 @@ class MemberStructure;
 class xbgMipFile {
 public:
 	void open(IBinaryArchive &fp);
+	void registerMembers(MemberStructure &ms);
+
+	uint32_t unk1;
+	Vector<xbgFile::SGfxBuffers> buffers;
 };
 
