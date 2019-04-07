8c0af8728f6ca042f054d4c3ff68aee4869beda6
Jonathan Scott <j301scott@gmail.com>
2019-04-07 15:26:09 -0700

    Read unknown files

--- DisruptEditor/FileHandler.cpp
+++ DisruptEditor/FileHandler.cpp
@@ -113,6 +113,51 @@ std::string FH::getReverseFilename(uint32_t hash) {
 	return it->path;
 }
 
+static void handleUnknownPath(const char* base, std::unordered_map<uint32_t, std::string> &unknownFiles) {
+	tfDIR dir;
+	tfDirOpen(&dir, base);
+	while (dir.has_next) {
+		tfFILE file;
+		tfReadFile(&dir, &file);
+
+		if (!file.is_dir && strlen(file.name) > 8) {
+			file.name[8] = '\0';
+
+			uint32_t hash = std::stoul(file.name, NULL, 16);
+			if (unknownFiles.count(hash) == 0) {
+				unknownFiles[hash] = file.path;
+			}
+
+		}
+
+		tfDirNext(&dir);
+	}
+	tfDirClose(&dir);
+}
+
+static std::unordered_map<uint32_t, std::string> genListOfUnknown(const std::string &path) {
+	std::string unknownPath = path + "__UNKNOWN/";
+	std::unordered_map<uint32_t, std::string> unknownFiles;
+	if (!PathFileExistsA(unknownPath.c_str())) return unknownFiles;
+
+	tfDIR dir;
+	tfDirOpen(&dir, unknownPath.c_str());
+	while (dir.has_next) {
+		tfFILE file;
+		tfReadFile(&dir, &file);
+
+		if (file.is_dir && strcmp(file.name, ".") != 0 && strcmp(file.name, ".."))
+			handleUnknownPath(file.path, unknownFiles);
+
+		tfDirNext(&dir);
+	}
+	tfDirClose(&dir);
+
+	return unknownFiles;
+}
+
+static std::unordered_map<std::string, std::unordered_map<uint32_t, std::string> > unknownFileMap;
+
 std::string FH::getAbsoluteFilePath(const std::string &path) {
 	std::string fullPath = settings.patchDir + path;
 	if (PathFileExistsA(fullPath.c_str()))
@@ -125,17 +170,41 @@ std::string FH::getAbsoluteFilePath(const std::string &path) {
 
 	//Search Unknown Files
 	uint32_t hash = Hash::getFilenameHash(path);
+	auto it = unknownFileMap[settings.patchDir].find(hash);
+	if (it != unknownFileMap[settings.patchDir].end())
+		return it->second;
+
+	for (const std::string &base : settings.searchPaths) {
+		it = unknownFileMap[base].find(hash);
+		if (it != unknownFileMap[base].end())
+			return it->second;
+	}
 
 	return std::string();
 }
 
 std::string FH::getAbsoluteFilePath(uint32_t path) {
-	auto it = DB::instance().getFileByHash(path);
-	if (it)
-		return getAbsoluteFilePath(it->path);
+	auto itb = DB::instance().getFileByHash(path);
+	if (itb)
+		return getAbsoluteFilePath(itb->path);
+
+	//Search Unknown Files
+	uint32_t hash = path;
+	auto it = unknownFileMap[settings.patchDir].find(hash);
+	if (it != unknownFileMap[settings.patchDir].end())
+		return it->second;
+
+	for (const std::string &base : settings.searchPaths) {
+		it = unknownFileMap[base].find(hash);
+		if (it != unknownFileMap[base].end())
+			return it->second;
+	}
 
 	return "";
 }
 
 void FH::Init() {
+	unknownFileMap[settings.patchDir] = genListOfUnknown(settings.patchDir);
+	for (const std::string &base : settings.searchPaths)
+		unknownFileMap[base] = genListOfUnknown(base);
 }
